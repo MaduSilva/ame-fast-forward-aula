2bfc8aa6511e07bd8771309a388eb86323d09188
Maria Eduarda <maria.dsilva@amedigital.com>
2022-09-24 10:00:59 -0300

    creating comment entity

--- src/comments/entities/comment.entity.ts
+++ src/comments/entities/comment.entity.ts
@@ -1,1 +1,8 @@
-export class Comment {}
+//Entitys - Entidades
+//Aqui vão ser declarados os campos que nosso resource terá
+//Precisa ser exatamente o que o banco de dados irá receber
+export class Comment {
+  id: number;
+  user_id: string;
+  comment: string;
+}
