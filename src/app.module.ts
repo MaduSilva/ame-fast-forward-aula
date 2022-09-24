9434de1c73461d3003c5352fdb65733375fd9ff9
Maria Eduarda <maria.dsilva@amedigital.com>
2022-09-24 09:32:52 -0300

    update: instructions to create module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,10 @@ import { CommentsModule } from './comments/comments.module';
 //vamos importar tudo o que precisamos aqui dentro
 //cada unidade deve ter seu próprio module, pra não sobrecarregar o módulo "root" principal
 
-//comando para criar novo module - nest g resource
+//comando para criar novo module - nest g resource 
+//What name would you like to use for this resource (plural, e.g., "users")? >>>>>Comments
+//What transport layer do you use? >>>>> REST API
+//Would you like to generate CRUD entry points? >>>> Yes
 @Module({
   imports: [CommentsModule],
   controllers: [],
