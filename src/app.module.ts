76691ed36add54141c9f558538383bb48f80c657
Maria Eduarda <maria.dsilva@amedigital.com>
2022-09-24 09:29:36 -0300

    creating comments module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+//Module - organizar a estrutura da nossa aplicação
 
+import { Module } from '@nestjs/common';
+import { CommentsModule } from './comments/comments.module';
+
+//vamos importar tudo o que precisamos aqui dentro
+//cada unidade deve ter seu próprio module, pra não sobrecarregar o módulo "root" principal
+
+//comando para criar novo module - nest g resource
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CommentsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
